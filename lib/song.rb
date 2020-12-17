51b5e5b0653a73e2e55c83ece4a5d07dc4119fed
bambi teague <teeegsa@gmail.com>
2020-12-17 10:52:23 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create_by_name(name)  # is saying do this(if it is true) or that (if the first thing is not true and the second thing is true
+      find_by_name(name) || create_by_name(name)  # is saying do this(if it is true) or that (if the first thing is not true and the second thing is true
   end
   
   def self.alphabetical
